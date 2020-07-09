29702e08a1da75bd0a1fbc754e26e2c0a0cc94b5
Sanity.io <hello@sanity.io>
2020-07-09 14:02:55 +0200

    Initial commit

---
+++ web/README.md
@@ -0,0 +1,1 @@
+# sanity-tattoo-portfolio-web
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '82qhyww4',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
